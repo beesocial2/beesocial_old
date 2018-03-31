55be55f008f2cb0f5967ba2f26192a339f42473e
BctfN0HUK7Yg <oksimets@yandex.ru>
2018-03-31 18:08:53 +0300

    изменил меню, добавил шаблон диалога для регистрации (надо редактировать), в скрипты добавил валидацию данных

--- app.js
+++ app.js
@@ -2,7 +2,35 @@
 var app = new Vue({
 		el: '#app',
 		data: {
-			register:false
-			
-		}
+			registered:false,
+			registerdialog:false,
+			valid: true,
+			name: '',
+			nameRules: [
+				v => !!v || 'Name is required',
+				v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+			],
+			email: '',
+			emailRules: [
+				v => !!v || 'E-mail is required',
+				v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+			],
+			select: null,
+			items: [
+				'Item 1',
+				'Item 2',
+				'Item 3',
+				'Item 4'
+			],
+			checkbox: false,
+		},
+		
+		methods: {
+			submit () {
+		
+			},
+			clear () {
+			this.$refs.form.reset()
+			}
+		},
 });
