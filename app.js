2dd449d3b03119325e6d6b98be0c01f251f254a8
Sing Painter <mail@erlan.xyz>
2018-04-01 07:29:12 +0300

    * форматирование кода * обновление формы регистрации

--- app.js
+++ app.js
@@ -1,51 +1,49 @@
-
 var app = new Vue({
 		el: '#app',
 		data: {
-			page:"about",
-			registered:false,
-			registerdialog:false,
-			projlist:[],
+			page: 'about',
+			registered: false,
+			registerdialog: false,
+			projlist: [],
 			
 			valid: false,
 			
-			name: '',
-			nameRules: [
-				v => !!v || 'Имя и фамилию необходимо ввести',
-				v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+			select: null,
+			items: [
+				'НКО',
+				'Спонсор',
+				'Волонтёр'
 			],
 			email: '',
 			emailRules: [
 				v => !!v || 'Эл.почта необходимо ввести',
 				v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Эл.почта необходимо ввести'
 			],
-			select: null,
-			items: [
-				'Item 1',
-				'Item 2',
-				'Item 3',
-				'Item 4'
+			login: '',
+			loginRules: [
+				v => !!v || 'Логин необходимо ввести',
+				v => (v && v.length <= 5) || 'Name must be less than 5 characters'
 			],
-			checkbox: false,
+			password: '',
+			passwordRules: [
+				v => !!v || 'Пароль необходимо ввести',
+				v => (v && v.length <= 5) || 'Name must be less than 5 characters'
+			],
 		},
 		
 		methods: {
 			showprojects: function (event){
-				this.page="projects";
+				this.page = 'projects';
 				console.log('переход на проекты');
 				
 			},
 			showresources: function (event){
-				this.page="resources";
+				this.page = 'resources';
 				console.log('переход на ресурсы');
 				
 			},
 			submit: function (event) {
 				
-			},
-			clear:function (event) {
-				valid=false;
-				this.$refs.form.reset()
 			}
 		},
 });
