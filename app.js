9d6675b0391b712032de37a7fae2f5dc093e18ab
Sing Painter <mail@erlan.xyz>
2018-04-01 11:18:27 +0300

    + сделана авторизация для блокчейна

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099
 localStorage && localStorage.wif ? window.wif = localStorage.wif : window.wif = '';
 localStorage && localStorage.username ? window.username = localStorage.username : window.username = '';
 
+let callbackAuth;
+let auth = function(callback) {
+	if (wif && username) callback();
+	else {
+		app.loginDialog = true;
+		callbackAuth = callback;
+	}
+};
+
 var app = new Vue({
 		el: '#app',
 		data: {
 			page: 'about',
 			registered: false,
+			loginDialog: false,
 			registerdialog: false,
 			newResourceDialog: false,
 			projlist: [],
@@ -28,15 +38,7 @@ var app = new Vue({
 				v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Эл.почта необходимо ввести'
 			],
 			login: '',
-			loginRules: [
-				v => !!v || 'Логин необходимо ввести',
-				v => (v && v.length <= 5) || 'Name must be less than 5 characters'
-			],
 			password: '',
-			passwordRules: [
-				v => !!v || 'Пароль необходимо ввести',
-				v => (v && v.length <= 5) || 'Name must be less than 5 characters'
-			],
 			title: '',
 			description: '',
 			howGet: '',
@@ -72,26 +74,49 @@ var app = new Vue({
 				});
 			},
 			submit: function(event) {
-				let parentAuthor = '';
-				let parentPermlink = 'beesocial';
-				let permlink = Date.now().toString();
-				let title = this.title;
-				let body = '<h1><a href="">Этот пост был создан на платформе BeeSocial</a></h1>' + this.description;
-				let jsonMetadata = {
-					title: this.title,
-					description: this.description,
-					howGet: this.howGet,
-					contacts: this.contacts,
-					combs: this.combs
-				};
-				golos.broadcast.comment(wif, parentAuthor, parentPermlink, username, permlink, title, body, jsonMetadata, function (err, result) {
-					//console.log(err, result);
-					if ( ! err) {
-						//console.log('post: ', result);
-						//window.location.hash = username + '/' + str;
-						app.newResourceDialog = false;
-					} else console.error(err);
-				});
+				if (app.loginDialog) {
+					const roles = ['posting'];
+					let keys = golos.auth.getPrivateKeys(this.login, this.password, roles);
+					golos.api.getAccounts([this.login], function(err, response) {
+						if (response && response[0] && response[0].posting.key_auths[0][0] == keys.postingPubkey) {
+							wif = keys.posting;
+							localStorage.wif = wif;
+							let resultWifToPublic = golos.auth.wifToPublic(wif);
+							let result = golos.api.getKeyReferences([resultWifToPublic], function(err, result) {
+								if (result && result[0]) {
+									username = result[0][0];
+									localStorage.username = username;
+									app.loginDialog = false;
+									if (callbackAuth) callbackAuth();
+								} else if (err) console.error(err);
+							});
+						} else alert('Неверный логин или пароль!');
+					});
+				}
+				else if (app.newResourceDialog) {
+					let parentAuthor = '';
+					let parentPermlink = 'beesocial';
+					let permlink = Date.now().toString();
+					let title = this.title;
+					let body = '<h1><a href="">Этот пост был создан на платформе BeeSocial</a></h1>' + this.description;
+					let jsonMetadata = {
+						title: this.title,
+						description: this.description,
+						howGet: this.howGet,
+						contacts: this.contacts,
+						combs: this.combs
+					};
+					auth(function() {
+						golos.broadcast.comment(wif, parentAuthor, parentPermlink, username, permlink, title, body, jsonMetadata, function (err, result) {
+							//console.log(err, result);
+							if ( ! err) {
+								//console.log('post: ', result);
+								//window.location.hash = username + '/' + str;
+								app.newResourceDialog = false;
+							} else console.error(err);
+						});
+					});
+				}
 			}
 		},
 });
